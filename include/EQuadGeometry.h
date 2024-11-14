4c104adc72e607414137c571e5fd4f938fb876a7
Eual11 <uchihaeual11@gmail.com>
2024-11-14 21:10:29 +0300

    Add new geometry: Quad Geometry

---
+++ include/EQuadGeometry.h
@@ -0,0 +1,9 @@
+#ifndef __EQUADGEOMETRY__
+#define __EQUADGEOMETRY__
+#include "EBufferGeometry.h"
+
+class EQuadGeometry : public EBufferGeometry {
+public:
+  EQuadGeometry(float width = 1.0f, float height = 1.0f);
+};
+#endif
---
+++ src/EQuadGeometry.cpp
@@ -0,0 +1,20 @@
+#include "../include/EQuadGeometry.h"
+
+EQuadGeometry::EQuadGeometry(float w, float h) {
+  // Define vertices in NDC to cover the whole screen
+  vertices = {
+      {{-1.0f, -1.0f, 0.0f}, {0, 0, 1}, {0, 0}}, // bottom-left
+      {{1.0f, -1.0f, 0.0f}, {0, 0, 1}, {1, 0}},  // bottom-right
+      {{1.0f, 1.0f, 0.0f}, {0, 0, 1}, {1, 1}},   // top-right
+      {{-1.0f, 1.0f, 0.0f}, {0, 0, 1}, {0, 1}}   // top-left
+  };
+
+  indices = {
+      0, 1, 2, // first triangle
+      2, 3, 0  // second triangle
+  };
+
+  // OpenGL setup
+  glGenVertexArrays(1, &VAO);
+  setupGeometry();
+}
